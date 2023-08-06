4c0c31e94eca4d4f9ce166bd3219c282c9561b98
MahamudulHimel <mhhimel589@gmail.com>
2023-08-06 23:40:22 +0600

    database added

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,11 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-    user : "postgres",
-    password : "qwerty",
-    host : "localhost",
+    user : "blpwbioq",
+    password : "C_HN38OY4xFHR9gGv29ChX6j5ouySzsp",
+    host : "trumpet.db.elephantsql.com",
     port : 5432 ,
-    database : "classes"
+    database : "blpwbioq"
 });
 
 module.exports = pool;
